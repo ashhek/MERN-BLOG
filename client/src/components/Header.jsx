dfa27eb1e2f366784578dfaea891b0f9c53ef62e
Abhishek <abhishek.2101150ec@iiitbh.ac.in>
2024-03-20 01:24:08 +0530

    Create and run the server

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -10,8 +10,7 @@ export default function Header() {
     <Navbar className="border-b-2">
       <Link
         to="/"
-        className="self-center whitespace-nowrap text-sm
-      sm:text-xl font-semibold dark:text-white"
+        className="self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white"
       >
         <span
           className="px-2 py-2 bg-gradient-to-r from-indigo-500
@@ -34,12 +33,12 @@ export default function Header() {
       <Button className="w-12 h-10 lg:hidden" color="gray" pill>
         <AiOutlineSearch />
       </Button>
-      <div clasName="flex gap-2 md:order-2">
+      <div className="flex gap-2 md:order-2">
         <Button className="w-12 h-10 hidden sm:inline" color="gray" pil>
           <FaMoon />
         </Button>
         <Link to="/sign-in">
-          <Button gradientDuoTone="purpleToBlue">Sign In</Button>
+          <Button gradientDuoTone="purpleToBlue" outline>Sign In</Button>
         </Link>
         <Navbar.Toggle />
       </div>
